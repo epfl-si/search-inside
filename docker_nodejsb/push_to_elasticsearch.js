689aa2e29cd574e829feb0df428d94ed318ea0ad
strikerpt <hugo.valente1996@gmail.com>
2021-03-18 13:45:38 +0100

    wip

--- docker_nodejsb/push_to_elasticsearch.js
+++ docker_nodejsb/push_to_elasticsearch.js
@@ -55,7 +55,7 @@ const write_data_medias = async (file_name, data, source_media) => {
   //Write the data into elasticsearch
   return axios({
     method: "POST",
-    url: `https://searchinside-elastic.epfl.ch/_doc?pipeline=attachment`,
+    url: `https://searchinside-elastic.epfl.ch/inside_temp/_doc?pipeline=attachment`,
     data: {
       url: `${source_media}`,
       rights: `test`,
@@ -69,7 +69,7 @@ const write_data_pages = async (link_page, title_page, StripHTMLBreakLines) => {
   //Write the data into elasticsearch
   return axios({
     method: "POST",
-    url: `https://searchinside-elastic.epfl.ch/_doc`,
+    url: `https://searchinside-elastic.epfl.ch/inside_temp/_doc`,
     data: {
       url: `${link_page}`,
       title: `${title_page}`,
@@ -84,7 +84,7 @@ const delete_inside_temp = async () => {
   console.log("delete_inside_temp");
   return axios({
     method: "DELETE",
-    url: `https://searchinside-elastic.epfl.ch`,
+    url: `https://searchinside-elastic.epfl.ch/isnide_temp`,
   });
 };
 
@@ -93,7 +93,7 @@ const delete_inside = async () => {
   console.log("delete_inside");
   return axios({
     method: "DELETE",
-    url: `https://searchinside-elastic.epfl.ch`,
+    url: `https://searchinside-elastic.epfl.ch/inside`,
   });
 };
 
@@ -102,7 +102,7 @@ const create_inside_temp = async () => {
   console.log("create_inside_temp");
   return axios({
     method: "POST",
-    url: `https://searchinside-elastic.epfl.ch/_doc/`,
+    url: `https://searchinside-elastic.epfl.ch/inside_temp/_doc/`,
     data: {
       mappings: {
         properties: {
@@ -136,7 +136,7 @@ const create_attachment_field = async () => {
   console.log("create_attachement_field");
   return axios({
     method: "PUT",
-    url: `https://searchinside-elastic.epfl.ch/_ingest/pipeline/attachment`,
+    url: `https://searchinside-elastic.epfl.ch/inside_temp/_ingest/pipeline/attachment`,
     data: {
       description: "Extract attachment information",
       processors: [
