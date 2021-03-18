71ff6f99defbcd4a3927139a6175bb9ff112e86d
strikerpt <hugo.valente1996@gmail.com>
2021-03-18 11:55:16 +0100

    wip

--- docker_nodejsb/push_to_elasticsearch.js
+++ docker_nodejsb/push_to_elasticsearch.js
@@ -55,7 +55,7 @@ const write_data_medias = async (file_name, data, source_media) => {
   //Write the data into elasticsearch
   return axios({
     method: "POST",
-    url: `http://172.30.64.166:9200/_doc?pipeline=attachment`,
+    url: `http://search-inside-elastic.wwp-test.svc/_doc?pipeline=attachment`,
     data: {
       url: `${source_media}`,
       rights: `test`,
@@ -69,7 +69,7 @@ const write_data_pages = async (link_page, title_page, StripHTMLBreakLines) => {
   //Write the data into elasticsearch
   return axios({
     method: "POST",
-    url: `http://172.30.64.166:9200/_doc`,
+    url: `http://search-inside-elastic.wwp-test.svc/_doc`,
     data: {
       url: `${link_page}`,
       title: `${title_page}`,
@@ -84,7 +84,7 @@ const delete_inside_temp = async () => {
   console.log("delete_inside_temp");
   return axios({
     method: "DELETE",
-    url: `http://172.30.64.166:9200`,
+    url: `http://search-inside-elastic.wwp-test.svc`,
   });
 };
 
@@ -93,7 +93,7 @@ const delete_inside = async () => {
   console.log("delete_inside");
   return axios({
     method: "DELETE",
-    url: `http://172.30.64.166:9200`,
+    url: `http://search-inside-elastic.wwp-test.svc`,
   });
 };
 
@@ -102,7 +102,7 @@ const create_inside_temp = async () => {
   console.log("create_inside_temp");
   return axios({
     method: "POST",
-    url: `http://172.30.64.166:9200/_doc/`,
+    url: `http://search-inside-elastic.wwp-test.svc/_doc/`,
     data: {
       mappings: {
         properties: {
@@ -136,7 +136,7 @@ const create_attachment_field = async () => {
   console.log("create_attachement_field");
   return axios({
     method: "PUT",
-    url: `http://172.30.64.166:9200/_ingest/pipeline/attachment`,
+    url: `http://search-inside-elastic.wwp-test.svc/_ingest/pipeline/attachment`,
     data: {
       description: "Extract attachment information",
       processors: [
@@ -157,7 +157,7 @@ const copy_inside_temp_to_inside = async () => {
   //Put the inside_temp into inside
   return axios({
     method: "POST",
-    url: `http://172.30.64.166:9200/_reindex`,
+    url: `http://search-inside-elastic.wwp-test.svc/_reindex`,
     data: {
       source: {
         index: "inside_temp",
