1f87b230f133b47bf40144d62617d874424b250d
William Belle <william.belle@gmail.com>
2022-05-30 09:24:09 +0200

    Add Tequila in api

--- docker_nodeapi/serverapi.js
+++ docker_nodeapi/serverapi.js
@@ -1,8 +1,34 @@
 const express = require('express');
+const expressSession = require('express-session');
+const passport = require('passport');
+const TequilaStrategy = require('passport-tequila').Strategy;
+
+// Use the TequilaStrategy within Passport.
+const tequila = new TequilaStrategy({
+  service: 'Search Inside',
+  request: ['displayname']
+});
+passport.use(tequila);
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
 
 const app = express();
 
-app.get('/', function (req, res) {
+app.use(expressSession({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', tequila.ensureAuthenticated, function (req, res) {
   res.send('Hello World');
 });
 
