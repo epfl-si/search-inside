60849755ef20997133d12afb06bbe4ff88d00016
Olivier Bieler <olivier.bieler@epfl.ch>
2022-06-03 17:16:24 +0200

    Remove getMediaBase64 + add timers

--- docker_nodejsb/push_to_elasticsearch.js
+++ docker_nodejsb/push_to_elasticsearch.js
@@ -17,8 +17,6 @@ if (process.env.RUNNING_HOST === 'local' || process.env.RUNNING_HOST === 'wwp-te
 
 // Get all pages data of a site
 const getPages = async (site) => {
-  console.log('getPages');
-
   const agent = new https.Agent({ rejectUnauthorized: false });
 
   let pages = [];
@@ -45,8 +43,6 @@ const getPages = async (site) => {
 
 // Get all medias data of a site
 const getMedias = async (site) => {
-  console.log('getMedias');
-
   const agent = new https.Agent({ rejectUnauthorized: false });
 
   let medias = [];
@@ -71,40 +67,32 @@ const getMedias = async (site) => {
   return medias;
 };
 
-// Get media in base64 encoded string
-const getMediaBase64 = (url) => {
-  const agent = new https.Agent({ rejectUnauthorized: false });
-
-  return axios
-    .get(url, {
-      responseType: 'arraybuffer', httpsAgent: agent, headers: { Host: 'inside.epfl.ch' }
-    })
-    .then(response => Buffer.from(response.data, 'binary').toString('base64'))
-    .catch((error) => {
-      console.log('Error getMediaBase64: ' + error);
-    });
-};
-
 // Index a media (pdf only for the moment)
 const indexMedia = async (fileName, sourceMedia) => {
-  console.log('indexMedia_debut ' + fileName + new Date().toISOString());
+  const agent = new https.Agent({ rejectUnauthorized: false });
 
   try {
     const sourceMediaTmp = sourceMedia.replace(/inside.epfl.ch/, insideHost);
 
-    const data = await getMediaBase64(sourceMediaTmp);
+    axios.get(sourceMediaTmp, {
+      responseType: 'arraybuffer', httpsAgent: agent, headers: { Host: 'inside.epfl.ch' }
+    })
+      .then((response) => {
+        const data = Buffer.from(response.data, 'binary').toString('base64');
 
-    axios.post(`${url}/inside_temp/_doc?pipeline=attachment`, {
-      url: `${sourceMedia}`,
-      title: `${fileName}`,
-      data: `${data}`,
-      rights: 'test'
-    }, { maxBodyLength: Infinity })
+        axios.post(`${url}/inside_temp/_doc?pipeline=attachment`, {
+          url: `${sourceMedia}`,
+          title: `${fileName}`,
+          data: `${data}`,
+          rights: 'test'
+        }, { maxBodyLength: Infinity })
+          .catch((error) => {
+            console.log('Error POST attachment: ' + error);
+          });
+      })
       .catch((error) => {
-        console.log('Error POST attachment: ' + error);
+        console.log('Error get media: ' + error);
       });
-
-    console.log('indexMedia_fin ' + fileName + new Date().toISOString());
   } catch (e) {
     console.log('Error indexMedia: ' + e);
   }
@@ -129,12 +117,88 @@ const indexPage = async (linkPage, titlePage, StripHTMLBreakLines) => {
   }
 };
 
+// Index all pages
+const indexAllPages = async () => {
+  console.time('indexAllPages');
+  try {
+    for (const site of sites) {
+      const pages = await getPages(site);
+
+      for (const page of pages) {
+        const linkPage = page.link;
+        const titlePage = htmlEntities.decode(page.title.rendered);
+
+        let contentPage = page.content.rendered;
+
+        // Convert HTML to beautiful text
+        contentPage = convert(contentPage, {
+          selectors: [
+            { selector: 'ul', options: { itemPrefix: '· ' } },
+            { selector: 'a', options: { ignoreHref: true } },
+            { selector: 'img', format: 'skip' },
+            { selector: 'h1', format: 'block' },
+            { selector: 'h2', format: 'block' },
+            { selector: 'h3', format: 'block' },
+            { selector: 'h4', format: 'block' },
+            { selector: 'h5', format: 'block' },
+            { selector: 'h6', format: 'block' },
+            { selector: 'th', format: 'block' },
+            { selector: 'td', format: 'block' }
+          ]
+        });
+
+        // Convert HTML entities to characters
+        contentPage = htmlEntities.decode(contentPage);
+        // Replace any whitespace character
+        contentPage = contentPage.replace(/\s/g, ' ');
+        // Fine tune ul rendered
+        contentPage = contentPage.replace(/ +·/g, ' ·');
+        // Delete multiple space
+        contentPage = contentPage.replace(/ +/g, ' ');
+
+        await indexPage(linkPage, titlePage, contentPage);
+      }
+    }
+  } catch (e) {
+    console.log('Error indexAllPages: ' + e);
+  }
+  console.timeEnd('indexAllPages');
+};
+
+// Index all medias
+const indexAllMedias = async () => {
+  console.time('indexAllMedias');
+  try {
+    for (const site of sites) {
+      let count = 0;
+      console.time('indexAllMedias (' + site + ')');
+      const medias = await getMedias(site);
+
+      for (const media of medias) {
+        const sourceMedia = media.source_url;
+
+        if (media.mime_type === 'application/pdf') {
+          count++;
+          const fileName = sourceMedia.match(/(?<=\/)[^/]*$/g);
+          await indexMedia(fileName, sourceMedia);
+        } else {
+          console.log('file is not a pdf: ' + sourceMedia);
+        }
+      }
+      console.timeEnd('indexAllMedias (' + site + ')');
+      console.log('total (pdf): ' + count);
+    }
+  } catch (e) {
+    console.log('Erreur get data from medias' + e);
+  }
+  console.timeEnd('indexAllMedias');
+};
+
 // Delete inside temp
 const deleteInsideTemp = async () => {
-  console.log('deleteInsideTemp_debut' + new Date().toISOString());
+  console.log('deleteInsideTemp');
   return axios
     .delete(`${url}/inside_temp`)
-    .then((result) => result)
     .catch((error) => {
       console.error('Error deleteInsideTemp: ' + error);
     });
@@ -142,9 +206,12 @@ const deleteInsideTemp = async () => {
 
 // Delete inside temp
 const deleteInside = async () => {
-  console.log('deleteInside_debut' + new Date().toISOString());
+  console.time('deleteInside');
   return axios
     .delete(`${url}/inside`)
+    .then((result) => {
+      console.timeEnd('deleteInside');
+    })
     .catch((error) => {
       console.error('Error deleteInside: ' + error);
     });
@@ -213,7 +280,7 @@ const createAttachmentField = async () => {
 
 // Copy inside temp into inside
 const copyInsideTempToInside = async () => {
-  console.log('copyInsideTempToInside_debut' + new Date().toISOString());
+  console.time('copyInsideTempToInside');
   // Put the inside_temp into inside
   return axios
     .post(`${url}/_reindex`, {
@@ -225,6 +292,7 @@ const copyInsideTempToInside = async () => {
       }
     })
     .then((result) => {
+      console.timeEnd('copyInsideTempToInside');
       console.log(result.data);
     })
     .catch((error) => {
@@ -232,81 +300,8 @@ const copyInsideTempToInside = async () => {
     });
 };
 
-// Index all pages
-const indexAllPages = async () => {
-  console.log('indexAllPages_debut' + new Date().toISOString());
-  try {
-    for (const site of sites) {
-      const pages = await getPages(site);
-
-      for (const page of pages) {
-        const linkPage = page.link;
-        const titlePage = htmlEntities.decode(page.title.rendered);
-
-        let contentPage = page.content.rendered;
-
-        // Convert HTML to beautiful text
-        contentPage = convert(contentPage, {
-          selectors: [
-            { selector: 'ul', options: { itemPrefix: '· ' } },
-            { selector: 'a', options: { ignoreHref: true } },
-            { selector: 'img', format: 'skip' },
-            { selector: 'h1', format: 'block' },
-            { selector: 'h2', format: 'block' },
-            { selector: 'h3', format: 'block' },
-            { selector: 'h4', format: 'block' },
-            { selector: 'h5', format: 'block' },
-            { selector: 'h6', format: 'block' },
-            { selector: 'th', format: 'block' },
-            { selector: 'td', format: 'block' }
-          ]
-        });
-
-        // Convert HTML entities to characters
-        contentPage = htmlEntities.decode(contentPage);
-        // Replace any whitespace character
-        contentPage = contentPage.replace(/\s/g, ' ');
-        // Fine tune ul rendered
-        contentPage = contentPage.replace(/ +·/g, ' ·');
-        // Delete multiple space
-        contentPage = contentPage.replace(/ +/g, ' ');
-
-        await indexPage(linkPage, titlePage, contentPage);
-      }
-
-      console.log('indexAllPages_fin' + new Date().toISOString());
-    }
-  } catch (e) {
-    console.log('Error indexAllPages: ' + e);
-  }
-};
-
-// Index all medias
-const indexAllMedias = async () => {
-  console.log('indexAllMedias_debut' + new Date().toISOString());
-  try {
-    for (const site of sites) {
-      const medias = await getMedias(site);
-
-      for (const media of medias) {
-        const sourceMedia = media.source_url;
-
-        if (media.mime_type === 'application/pdf') {
-          const fileName = sourceMedia.match(/(?<=\/)[^/]*$/g);
-          await indexMedia(fileName, sourceMedia);
-        } else {
-          console.log('file is not a pdf: ' + `${sourceMedia}`);
-        }
-      }
-      console.log('getDataFromMedias_fin' + new Date().toISOString());
-    }
-  } catch (e) {
-    console.log('Erreur get data from medias' + e);
-  }
-};
-
 const launchScript = async () => {
-  console.log('launchScript');
+  console.time('launchScript');
   await deleteInsideTemp();
   await delay(2000);
   await createInsideTemp();
@@ -320,6 +315,7 @@ const launchScript = async () => {
   await copyInsideTempToInside();
   await delay(2000);
   await deleteInsideTemp();
+  console.timeEnd('launchScript');
 };
 
 launchScript();
