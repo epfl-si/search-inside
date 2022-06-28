5387c66d16658f6b7ca12e8dac4f379e439aee9e
Olivier Bieler <olivier.bieler@epfl.ch>
2022-06-28 09:15:06 +0200

    Stop Elastic at end of build index

--- docker_elasticsearch/bootstrap-index-build.sh
+++ docker_elasticsearch/bootstrap-index-build.sh
@@ -5,7 +5,7 @@ set -e -x
 # Run Elastic
 ES_JAVA_OPTS="-Xms2g -Xmx2g" \
   elasticsearch -E xpack.security.enabled=false -E discovery.type=single-node \
-                -E xpack.security.http.ssl.enabled=false &
+                -E xpack.security.http.ssl.enabled=false -d -p /tmp/pid
 
 # Wait for Elastic
 while ! curl http://localhost:9200/_cat/health?h=st; do sleep 5; done
@@ -15,5 +15,9 @@ node /app/build_index.js
 
 # Check index
 curl http://localhost:9200/_cat/indices
+
+# Stop Elastic
+pkill -F /tmp/pid
+while ps aux | grep elasticsearch | grep -v grep; do sleep 5; done
 
 exit 0
