191e2a41559fdfee8700491db07431b9baf66650
Olivier Bieler <olivier.bieler@epfl.ch>
2022-07-01 10:38:23 +0200

    Config mem_lock (prevent memory swapping)

--- docker_elasticsearch/bootstrap-index-build.sh
+++ docker_elasticsearch/bootstrap-index-build.sh
@@ -5,7 +5,8 @@ set -e -x
 # Run Elastic
 ES_JAVA_OPTS="-Xms2g -Xmx2g" \
   elasticsearch -E xpack.security.enabled=false -E discovery.type=single-node \
-                -E xpack.security.http.ssl.enabled=false -d -p /tmp/pid
+                -E xpack.security.http.ssl.enabled=false -E bootstrap.memory_lock=true \
+                -d -p /tmp/pid
 
 # Wait for Elastic
 while ! curl http://localhost:9200/_cat/health?h=st; do sleep 5; done
