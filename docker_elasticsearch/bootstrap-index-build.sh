ca62d4ab31ee141fb32537d9e12ac7b0d5ab6183
Olivier Bieler <olivier.bieler@epfl.ch>
2022-07-04 16:00:44 +0200

    Revert mem_lock on build stage

--- docker_elasticsearch/bootstrap-index-build.sh
+++ docker_elasticsearch/bootstrap-index-build.sh
@@ -5,7 +5,7 @@ set -e -x
 # Run Elastic
 ES_JAVA_OPTS="-Xms2g -Xmx2g" \
   elasticsearch -E xpack.security.enabled=false -E discovery.type=single-node \
-                -E xpack.security.http.ssl.enabled=false -E bootstrap.memory_lock=true \
+                -E xpack.security.http.ssl.enabled=false \
                 -d -p /tmp/pid
 
 # Wait for Elastic
