1820f6b626dbd447627aa845028ffac885db0d8d
Olivier Bieler <olivier.bieler@epfl.ch>
2022-07-22 16:28:05 +0200

    (Re)index media

--- docker_elasticsearch/build_index.js
+++ docker_elasticsearch/build_index.js
@@ -391,7 +391,7 @@ const build = async () => {
   await createUserAndRole();
   await setKibanaPassword();
   await indexAllPages();
-  // await indexAllMedias();
+  await indexAllMedias();
   await delay(2000);
   console.log('\nBuild index sucessful.\n');
   console.log('*********************************************************');
